c0ffaf8522d5b694bf8c39b7e24d8f8f8a170b60
Nicolas de Marqué Fromentin <nicolas.demarque@gmail.com>
2012-04-12 16:23:46 +0200

    Fin de la partie 3

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,5 +13,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),    
 )
+
+
